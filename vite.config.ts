c2934d9d403335706cf0131bcad889860f97a81e
ppxb <317842449@qq.com>
2026-01-11 20:59:58 +0800

    chore: add last build epoch

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+import { version as pkgVersion } from './package.json'
+
 const host = process.env.TAURI_DEV_HOST
+const platform = process.env.TAURI_ENV_PLATFORM
+process.env.VITE_APP_VERSION = pkgVersion
+
+if (process.env.NODE_ENV === 'production') {
+	process.env.VITE_APP_BUILD_EPOCH = new Date().getTime().toString()
+}
 
 export default defineConfig(async () => ({
 	plugins: [react()],
