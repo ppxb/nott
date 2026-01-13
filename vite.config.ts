60f926d3dd59bdfbaed887fa04443232f95d6d2c
ppxb <317842449@qq.com>
2026-01-13 10:13:48 +0800

    lint: eslint code

--- vite.config.ts
+++ vite.config.ts
@@ -9,39 +9,39 @@ const platform = process.env.TAURI_ENV_PLATFORM
 process.env.VITE_APP_VERSION = pkgVersion
 
 if (process.env.NODE_ENV === 'production') {
-	process.env.VITE_APP_BUILD_EPOCH = new Date().getTime().toString()
+  process.env.VITE_APP_BUILD_EPOCH = new Date().getTime().toString()
 }
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
-		},
-	},
-	clearScreen: false,
-	envPrefix: ['VITE_', 'TAURI_'],
-	server: {
-		port: 1420,
-		strictPort: true,
-		host: host || false,
-		hmr: host
-			? {
-					protocol: 'ws',
-					host,
-					port: 1421,
-			  }
-			: undefined,
-		watch: {
-			ignored: ['**/src-tauri/**'],
-		},
-	},
-	build: {
-		outDir: './dist',
-		target: platform === 'windows' ? 'chrome111' : 'safari16.4',
-		minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-		emptyOutDir: true,
-		chunkSizeWarningLimit: 1024,
-		sourcemap: !!process.env.TAURI_ENV_DEBUG,
-	},
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  clearScreen: false,
+  envPrefix: ['VITE_', 'TAURI_'],
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+  build: {
+    outDir: './dist',
+    target: platform === 'windows' ? 'chrome111' : 'safari16.4',
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1024,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
 })
