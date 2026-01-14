36353357f9c3511aba4513beb3af6a8d436a8bf9
ppxb <317842449@qq.com>
2026-01-14 14:11:47 +0800

    chore: update tailwind config may remove later

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,6 @@
 import type { Config } from 'tailwindcss'
-import typography from '@tailwindcss/typography'
 
 export default {
-  plugins: [typography],
   content: [
     './src/**/*.{ts,tsx}',
   ],
