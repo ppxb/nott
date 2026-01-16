9162defcb0874bfe5b782eea69258fa4f633b2c7
ppxb <317842449@qq.com>
2026-01-16 13:38:25 +0800

    chore: add platform check

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,26 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function isMac() {
+  return navigator.platform.includes('Mac')
+}
+
+export function getShortcutKey(key: string) {
+  if (`${key}`.toLowerCase() === 'mod') {
+    return isMac() ? '⌘' : 'Ctrl'
+  }
+  else if (`${key}`.toLowerCase() === 'alt') {
+    return isMac() ? '⌥' : 'Alt'
+  }
+  else if (`${key}`.toLowerCase() === 'shift') {
+    return isMac() ? '⇧' : 'Shift'
+  }
+  else {
+    return key
+  }
+}
+
+export function getShortcutKeys(keys: string[]) {
+  return keys.map(getShortcutKey).join(' ')
+}
