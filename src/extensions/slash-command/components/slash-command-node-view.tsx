500f9055d66cd506125fca214054b51466a97496
ppxb <317842449@qq.com>
2026-01-23 11:07:16 +0800

    chore: add slash command ui

--- src/extensions/slash-command/components/slash-command-node-view.tsx
+++ src/extensions/slash-command/components/slash-command-node-view.tsx
@@ -1,5 +1,14 @@
 import { SuggestionKeyDownProps } from '@tiptap/suggestion'
-import { useEffect, useImperativeHandle, useRef, useState } from 'react'
+import {
+	Fragment,
+	forwardRef,
+	useEffect,
+	useImperativeHandle,
+	useRef,
+	useState
+} from 'react'
+import { Label } from '@/components/ui/label'
+import { cn } from '@/lib/utils'
 import { useCommandList } from '@/store/command-list'
 import { useFilteredCommandList } from '../render-command-list'
 
@@ -118,4 +127,65 @@ function SlashCommandNodeView(props: any, ref: any) {
 	function setActiveItemRef(groupIndex: number, cmomandIndex: number, el: any) {
 		activeItemRefs.current[groupIndex * 1000 + cmomandIndex] = el
 	}
+
+	return (
+		<div
+			className="max-h-[min(80vh,24rem)] flex-wrap overflow-y-auto overflow-x-hidden rounded-md border! border-solid! border-border! bg-popover p-1 text-popover-foreground shadow-md outline-none"
+			data-nott-portal
+			ref={scrollContainer}
+		>
+			{commandQuery?.length ? (
+				<div className="grid min-w-48 grid-cols-1 gap-0.5">
+					{commandQuery?.map((group: any, groupIndex: any) => {
+						return (
+							<Fragment key={`slash-${group.title}`}>
+								<Label className="mx-[4px] mb-[4px] mt-[8px] text-[0.65rem]! uppercase">
+									{group.title}
+								</Label>
+
+								{group.commands.map((command: any, commandIndex: any) => {
+									return (
+										<button
+											key={`command-${commandIndex}`}
+											onClick={() =>
+												createCommandClickHandler(groupIndex, commandIndex)
+											}
+											ref={el => setActiveItemRef(groupIndex, commandIndex, el)}
+											className={cn(
+												'flex w-full items-center gap-3 rounded-sm border-none! bg-transparent! px-2 py-1.5 text-left text-sm text-foreground !outline-none transition-colors hover:!bg-accent',
+												{
+													'bg-item-active':
+														selectedGroupIndex === groupIndex &&
+														selectedCommandIndex === commandIndex
+												}
+											)}
+										>
+											{command.iconUrl && (
+												<img alt="" className="size-6" src={command.iconUrl} />
+											)}
+
+											{/* {command.iconName && (
+												<IconComponent
+													className="mr-1! text-lg!"
+													name={command.iconName}
+												/>
+											)} */}
+
+											{command.label}
+										</button>
+									)
+								})}
+							</Fragment>
+						)
+					})}
+				</div>
+			) : (
+				<div className="p-3">
+					<span className="text-xs text-foreground">No Result</span>
+				</div>
+			)}
+		</div>
+	)
 }
+
+export default forwardRef(SlashCommandNodeView)
