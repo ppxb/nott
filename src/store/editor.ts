ef53271911ed5a4a6210845f1c03385c5adf68d6
ppxb <317842449@qq.com>
2026-01-24 12:16:53 +0800

    fix: useEditorInstance return type

--- src/store/editor.ts
+++ src/store/editor.ts
@@ -1,44 +1,54 @@
 import type { Editor } from '@tiptap/core'
-import { useCurrentEditor, useEditorState as useTiptapEditorState } from '@tiptap/react'
+import {
+	useCurrentEditor,
+	useEditorState as useTiptapEditorState
+} from '@tiptap/react'
 import { useMemo } from 'react'
 import { createSignal, useSetSignal, useSignalValue } from 'reactjs-signal'
 
 function useTiptapEditor(providedEditor?: Editor | null) {
-  const { editor: coreEditor } = useCurrentEditor()
-  return useMemo(
-    () => providedEditor || coreEditor,
-    [providedEditor, coreEditor],
-  )
+	const { editor: coreEditor } = useCurrentEditor()
+	const editor = useMemo(
+		() => providedEditor || coreEditor,
+		[providedEditor, coreEditor]
+	)
+
+	if (!editor) {
+		throw new Error(
+			'Editor not found. Make sure the component is used within EditorProvider.'
+		)
+	}
+	return editor
 }
 
-export function useEditorInstance(providedEditor?: Editor | null) {
-  return useTiptapEditor(providedEditor)
+export function useEditorInstance(providedEditor?: Editor) {
+	return useTiptapEditor(providedEditor)
 }
 
-export function useEditorState(providedEditor?: Editor | null) {
-  const editor = useTiptapEditor(providedEditor)
+export function useEditorState(providedEditor?: Editor) {
+	const editor = useTiptapEditor(providedEditor)
 
-  return useTiptapEditorState({
-    editor,
-    selector: context => context.editor?.state,
-  })
+	return useTiptapEditorState({
+		editor,
+		selector: context => context.editor?.state
+	})
 }
 
-export function useCanCommand(providedEditor?: Editor | null) {
-  const editor = useTiptapEditor(providedEditor)
+export function useCanCommand(providedEditor?: Editor) {
+	const editor = useTiptapEditor(providedEditor)
 
-  return useTiptapEditorState({
-    editor,
-    selector: context => context.editor?.can,
-  })
+	return useTiptapEditorState({
+		editor,
+		selector: context => context.editor?.can
+	})
 }
 
 const editorEditableSignal = createSignal(false)
 
 export function useEditorEditable() {
-  return useSignalValue(editorEditableSignal)
+	return useSignalValue(editorEditableSignal)
 }
 
 export function useSetEditorEditable() {
-  return useSetSignal(editorEditableSignal)
+	return useSetSignal(editorEditableSignal)
 }
