3b9d8d71e941ad9ba80fadbdda437d40b1841ebc
ppxb <317842449@qq.com>
2026-01-25 11:17:20 +0800

    chore: add GenericOptions type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,16 @@
 export interface ButtonViewReturnComponentProps {
-  action?: (value?: any) => void
-  isActive?: () => boolean
-  icon?: any
-  tooltip?: string
-  [x: string]: any
+	action?: (value?: any) => void
+	isActive?: () => boolean
+	icon?: any
+	tooltip?: string
+	[x: string]: any
+}
+
+// TODO: SHOULD FIX BUTTON TYPE
+export interface GenericOptions<T> {
+	divider: boolean
+	spacer: boolean
+	button: any
+	toolbar?: boolean
+	shortcutKeys?: string[] | string[][]
 }
