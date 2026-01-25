d39b87e8258d6bd5f55bcd67659bb810e701ec03
ppxb <317842449@qq.com>
2026-01-25 11:17:27 +0800

    chore: add heading icon

--- src/components/icons/icons.ts
+++ src/components/icons/icons.ts
@@ -1,9 +1,25 @@
 import {
-  GripVertical,
-  Plus,
+	ChevronDown,
+	GripVertical,
+	Heading1,
+	Heading2,
+	Heading3,
+	Heading4,
+	Heading5,
+	Heading6,
+	Plus,
+	Type
 } from 'lucide-react'
 
 export const icons = {
-  Grip: GripVertical,
-  Plus,
+	Grip: GripVertical,
+	Plus,
+	ChevronDown,
+	Heading1,
+	Heading2,
+	Heading3,
+	Heading4,
+	Heading5,
+	Heading6,
+	Text: Type
 }
